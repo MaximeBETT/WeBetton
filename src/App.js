40ec03a7e2c48d4fdee09c51038f7fb46427e730
Maxime BETTON <156648557+MaximeBET@users.noreply.github.com>
2025-06-22 19:14:32 +0200

    test modif

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to My Feur</h1>
+        <h1>je fais des tests</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
