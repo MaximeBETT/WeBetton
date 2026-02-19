d51a8259276953c8acf9ff27916d6139694c8d98
Maxime BETTON <156648557+MaximeBET@users.noreply.github.com>
2026-02-19 18:37:08 +0100

    modif site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ import './styles/App.css';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="app">
         <Header />
         <main className="main-content">
